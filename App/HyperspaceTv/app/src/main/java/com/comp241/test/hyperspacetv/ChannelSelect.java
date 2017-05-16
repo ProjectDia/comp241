94f5255b72d70d650242ee7ee8c9ca6d7530a872
ProjectDia <nicholastotman@gmail.com>
2017-05-16 23:35:50 +1200

    brae16/5

---
+++ App/HyperspaceTv/app/src/main/java/com/comp241/test/hyperspacetv/ChannelSelect.java
@@ -0,0 +1,17 @@
+package com.comp241.test.hyperspacetv;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChannelSelect extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_channel_select);
+    }
+
+    public void boi(){
+
+    }
+}
