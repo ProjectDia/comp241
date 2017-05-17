3a8d5c0518e8d2cae5316cd669b96f562195c063
ProjectDia <nicholastotman@gmail.com>
2017-05-17 13:38:07 +1200

    brae17/5

--- App/HyperspaceTv/app/src/main/java/com/comp241/test/hyperspacetv/ChannelSelect.java
+++ App/HyperspaceTv/app/src/main/java/com/comp241/test/hyperspacetv/ChannelSelect.java
@@ -2,6 +2,9 @@ package com.comp241.test.hyperspacetv;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import java.io.IOException;
 
 public class ChannelSelect extends AppCompatActivity {
 
@@ -11,7 +14,19 @@ public class ChannelSelect extends AppCompatActivity {
         setContentView(R.layout.activity_channel_select);
     }
 
-    public void boi(){
+    public void onClickOne(View view){
+        try {
+            ClientApp.main(new String[] {"241","1"});
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
+    public void onClickTick(View view){
+        setContentView(R.layout.activity_show_data);
+    }
+
+    public void onClickChannel(View view){
+        setContentView(R.layout.activity_channel_select);
     }
 }
