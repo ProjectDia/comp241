a893f186608a66cd46761c2dd408927c0cc7c067
ProjectDia <nicholastotman@gmail.com>
2017-05-30 18:42:52 +1200

    Update ClientApp.java

--- Server/ClientApp.java
+++ Server/ClientApp.java
@@ -6,27 +6,27 @@ public class ClientApp{
 		Socket socc = null;
 		PrintWriter out = null;
 		BufferedReader in = null;
-		if(args.length != 2){
-			System.out.println("please specify a port followed by your channel ");
+		if(args.length != 1){
+			System.out.println("please specify a channel ");
 			return;
 		}
-		int portNumber = Integer.parseInt(args[0]);
+		int portNumber = 1234;
 
 		try{
-			socc = new Socket("127.0.0.1",portNumber);
-			out = new PrintWriter(socc.getOutputStream(), true); // printwriter for output
-			in = new BufferedReader(new InputStreamReader(socc.getInputStream())); // stream for reading from server
+			socc = new Socket("f.l0.nz", portNumber);
+			out = new PrintWriter(socc.getOutputStream(), true); //printwriter for output
+			in = new BufferedReader(new InputStreamReader(socc.getInputStream())); //stream for reading from server
 		} catch (UnknownHostException e) {
-            System.err.println("Unknown host: " + args[0]); // no server
+            System.err.println("Unknown host"); //no server
             System.exit(1);
         } catch (IOException e) {
-            System.err.println("IO Exception for host " + args[0]);
+            System.err.println("IO Exception for host ");
             System.exit(1);
         }
 
         String fromServer;
         String fromUser;
-        out.println(args[1]);
+        out.println(args[0]);
         while((fromServer = in.readLine()) != null){ //while there is input from the server
         	System.out.println(fromServer);
         }
