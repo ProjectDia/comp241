cd4b7e792b5d66bf187bea10df49348e1eefa296
ProjectDia <nicholastotman@gmail.com>
2017-05-30 16:21:38 +1200

    Create ClientApp.java

---
+++ Server/ClientApp.java
@@ -0,0 +1,38 @@
+import java.net.*;
+import java.io.*;
+
+public class ClientApp{
+	public static void main(String[] args) throws IOException{
+		Socket socc = null;
+		PrintWriter out = null;
+		BufferedReader in = null;
+		if(args.length != 2){
+			System.out.println("please specify a port followed by your channel ");
+			return;
+		}
+		int portNumber = Integer.parseInt(args[0]);
+
+		try{
+			socc = new Socket("127.0.0.1",portNumber);
+			out = new PrintWriter(socc.getOutputStream(), true); // printwriter for output
+			in = new BufferedReader(new InputStreamReader(socc.getInputStream())); // stream for reading from server
+		} catch (UnknownHostException e) {
+            System.err.println("Unknown host: " + args[0]); // no server
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("IO Exception for host " + args[0]);
+            System.exit(1);
+        }
+
+        String fromServer;
+        String fromUser;
+        out.println(args[1]);
+        while((fromServer = in.readLine()) != null){ //while there is input from the server
+        	System.out.println(fromServer);
+        }
+
+        out.close();
+        in.close();
+        socc.close();
+	}
+}
